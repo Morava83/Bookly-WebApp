073e0eca378c7087e2f7dc79677c62685532e71f
Enoch Chan <enoch.enoch.chan@gmail.com>
2026-04-11 12:56:35 -0400

    Linked the template pages to app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def get_role_from_email(email):
     return None
 
 def hash_password(password):
-    return generate_password_hash(password)
+    return generate_password_hash(password, method="pbkdf2:sha256", salt_length=16)
 
 def verify_password(password_hash, password):
     return check_password_hash(password_hash, password)
