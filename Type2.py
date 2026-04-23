4dcdea6e116c6880fd7772c6a9ef7c97822f78b0
mmiladinovgenov <mmiladinovgenov@luc.edu>
2026-04-23 15:03:26 -0400

    Added Database helper functions to Type2

--- Type2.py
+++ Type2.py
@@ -1,12 +1,36 @@
-#Type2: Group Meeting
 from flask import Blueprint, request, current_app, jsonify
+import os
+import sqlite3
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+#-------Type 2: Group Meeting-------------
 
+#=======Setup========
 type2_blueprint = Blueprint('Type2', __name__)
 
+BASE_DIR = os.path.dirname(__file__)
+DB_PATH = os.path.join(BASE_DIR, "database", "bookly.db")
 
 #--------Media Query----------
-#LOGIC
+
+def get_db_connection():
+    conn = sqlite3.connect(DB_PATH)
+    conn.row_factory = sqlite3.Row
+    conn.execute("PRAGMA foreign_keys = ON")
+    return conn
+
 #Get Owner
+def get_owner(id):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("""
+                   SELECT *
+                   FROM User u 
+                   JOIN Owner o ON u.userId = o.userId 
+                   WHERE o.userId = ?
+                   """, (id,))
+
 
 #Get list of invited users
 
